79d61f97583a8076f2b84bbc87f7de720127477c
Redouxne <130865775+Redouxne@users.noreply.github.com>
2023-09-18 10:50:43 +0200

    Add files via upload

--- API météo.js
+++ API météo.js
@@ -1,16 +1,22 @@
-// Fonction pour obtenir la météo d'une ville spécifique
-function getWeather(city) {
-    const apiKey = "VOTRE_CLÉ_API";  // Inscrivez-vous sur OpenWeatherMap pour obtenir une clé API
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=fr`;
+// Coordonnées de Paris, France
+const latitude = 48.8566;
+const longitude = 2.3522;
 
-    fetch(endpoint)
-        .then(response => response.json())
-        .then(data => {
-            const temperature = data.main.temp;
-            const conditions = data.weather[0].description;
-            alert(`Il fait actuellement ${temperature}°C à ${city} avec ${conditions}.`);
-        })
-        .catch(error => {
-            alert("Désolé, je n'ai pas pu obtenir les informations météo pour cette ville.");
-        });
-}
+// URL de l'API Open-Meteo pour obtenir les prévisions météorologiques
+const endpoint = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
+
+// Requête à l'API
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        // Affichage des données météorologiques actuelles
+        console.log("Météo actuelle:", data.current_weather);
+
+        // Affichage des données météorologiques horaires
+        console.log("Prévisions horaires:", data.hourly);
+    })
+    .catch(error => {
+        console.error("Erreur lors de la récupération des données météorologiques:", error);
+    });
+
+    
