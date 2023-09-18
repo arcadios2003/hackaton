4b45aba8add298e6e37c91a1dedda19f70b0ceea
Arthur CATTY <34744791+arcadios2003@users.noreply.github.com>
2023-09-18 10:14:49 +0200

    hackaton.sql
    add sql insertion request

--- hackaton.sql
+++ hackaton.sql
@@ -1,27 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 18, 2023 at 08:02 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hackaton`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `log`
@@ -37,6 +19,6 @@ CREATE TABLE IF NOT EXISTS `log` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Requête insertion pour la bdd
+
+INSERT INTO log (user,message) Values(user,message)
